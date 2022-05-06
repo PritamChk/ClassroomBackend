3021be529f53b5bca8e9a2a6a0e34bc7140a5de9
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-05-07 02:12:29 +0530

    Student Profile Retrive working

--- classroom/urls.py
+++ classroom/urls.py
@@ -1,14 +1,19 @@
 from django.urls import path, include
+
 from rest_framework.routers import DefaultRouter
 
 from .views.student_view import user_category
+
+from .views.student_view import StudentViewSet
+
 # from .views.student_view import DynamicRetriveUser
 
-# student_router = DefaultRouter()
-# student_router.register('student',StudentViewSet,'student')
+student_router = DefaultRouter()
+student_router.register("student", StudentViewSet, "student")
 
-# urlpatterns = student_router.urls
 urlpatterns = [
     path("user-type/<uuid:id>", user_category, name="user-category")
     # path("user-type/<uuid:id>", DynamicRetriveUser.as_view())
 ]
+
+urlpatterns += student_router.urls
--- classroom/views/student_view.py
+++ classroom/views/student_view.py
@@ -2,44 +2,31 @@ from rest_framework.decorators import api_view
 from rest_framework.mixins import RetrieveModelMixin
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
+from rest_framework.generics import GenericAPIView
 
+from classroom.serializers.student import StudentReadSerializer
 from ..models import Student, Teacher
-from ..serializers.student import StudentReadSerializer
 
 
 @api_view(["GET"])
 def user_category(request, id):
     my_tags = ["User Category"]
     if Teacher.objects.select_related("user").filter(user__id=id).exists():
-        return Response({"user_type": "teacher"})
+        teacher = (
+            Teacher.objects.select_related("user").filter(user__id=id)
+            # .only("id")
+            .first()
+        )
+        return Response({"user_type": "teacher", "teacher_id": teacher.id})
     elif Student.objects.select_related("user").filter(user__id=id).exists():
-        return Response({"user_type": "student"})
+        student = (
+            Student.objects.select_related("user").filter(user__id=id)
+            # .only("id")
+            .first()
+        )
+        return Response({"user_type": "student", "student_id": student.id})
     else:
         return Response({"user_type": "user unknown"})
-
-
-# class DynamicRetriveUser(RetrieveModelMixin, GenericAPIView):
-#     my_tags = ["User Category"]
-#     http_methods = ["GET"]
-
-#     def get_queryset(self):
-#         id = self.kwargs["pk"]
-#         if Teacher.objects.select_related("user").filter(user__id=id).exists():
-#             return (
-#                 Teacher.objects.select_related("user")
-#                 .prefetch_related("classroom")
-#                 .all(pk=id)
-#             )
-#         elif Student.objects.select_related("user").filter(user__id=id).exists():
-#             return (
-#                 Student.objects.select_related("user")
-#                 .select_related("classroom")
-#                 .filter(pk=id)
-#             )
-
-#     def get_serializer_class(self):
-#         if Student.objects.select_related("user").filter(user__id=id).exists():
-#             return StudentReadSerializer
 
 
 class StudentViewSet(RetrieveModelMixin, GenericViewSet):
@@ -48,16 +35,8 @@ class StudentViewSet(RetrieveModelMixin, GenericViewSet):
     """
 
     my_tags = ["Student"]
+    queryset = Student.objects.select_related("user").select_related("classroom").all()
+    serializer_class = StudentReadSerializer
 
-    def get_queryset(self):
-        return Student.objects.select_related("classroom").select_related("user").all()
-
-    def get_serializer_class(self):
-        return StudentReadSerializer
-
-    def me(self):
-        q = (
-            Student.objects.select_related("classroom")
-            .select_related("user")
-            .get(pk=self.request.user.id)
-        )
+    def get_serializer_context(self):
+        return {"request": self.request}
