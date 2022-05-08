f0805f1e04828d8ef387474bfe19a008f28c84cb
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-05-08 16:56:13 +0530

    routers refactored

--- classroom/urls.py
+++ classroom/urls.py
@@ -1,51 +1,7 @@
 from django.urls import include, path
-from rest_framework_nested.routers import DefaultRouter, NestedDefaultRouter
 from termcolor import cprint
 
-from .views.student_view import (
-    AnnouncementForStudentsViewSet,
-    ClassroomForStudentViewSet,
-    SemesterForStudentViewSet,
-    StudentProfileViewSet,
-    SubjectsForStudentsViewSet,
-    user_category,
-)
+from .routers.students_urls import stud_urlpatterns
 
-student_router = DefaultRouter()
-student_router.register("student", StudentProfileViewSet, "student")
-
-
-classroom_router = DefaultRouter()
-classroom_router.register("classroom", ClassroomForStudentViewSet, "classroom")
-
-classroom_sems_router = NestedDefaultRouter(
-    classroom_router, "classroom", lookup="classroom"
-)
-classroom_sems_router.register("semester", SemesterForStudentViewSet, basename="sem")
-
-sem_subjects_router = NestedDefaultRouter(
-    classroom_sems_router, "semester", lookup="semester"
-)
-sem_subjects_router.register("subject", SubjectsForStudentsViewSet, basename="subject")
-
-
-subject_announcement_router = NestedDefaultRouter(
-    sem_subjects_router, "subject", lookup="subject"
-)
-subject_announcement_router.register(
-    "announcement", AnnouncementForStudentsViewSet, basename="announcement"
-)
-
-urlpatterns = [path("user-type/<uuid:id>", user_category, name="user-category")]
-
-
-urlpatterns += (
-    student_router.urls
-    + classroom_router.urls
-    + classroom_sems_router.urls
-    + sem_subjects_router.urls
-    + subject_announcement_router.urls
-)
-
-for url in urlpatterns:
-    cprint(url, "green")
+urlpatterns = []
+urlpatterns += stud_urlpatterns
