17262f15712dd6caedd050395ef42febea165f50
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-05-10 02:08:46 +0530

    Student Model Created

--- classroom/models.py
+++ classroom/models.py
@@ -258,53 +258,52 @@ class Semester(models.Model):
         return self.classroom.title
 
 
-# class Student(models.Model):
-#     """
-#     ## `Student`
-#     ---
-#         - user: takes User()
-#         - classroom: takes User()
-#     """
+class Student(models.Model):
 
-#     university_roll = models.PositiveBigIntegerField(
-#         _("University Roll"),
-#         help_text="Your University Roll No - (e.g. - 13071020030)",
-#         null=True,
-#         blank=False,
-#         default=randint(10, 9999999),
-#     )
-#     user = models.OneToOneField(
-#         User,
-#         on_delete=models.CASCADE,
-#         null=True,
-#         blank=True,
-#         related_name="student_profile",
-#     )
-#     classroom = models.ForeignKey(
-#         Classroom,
-#         on_delete=models.CASCADE,
-#         related_name="students",
-#         null=True,
-#         blank=True,
-#     )
+    university_roll = models.PositiveBigIntegerField(
+        _("University Roll"),
+        help_text="Your University Roll No - (e.g. - 13071020030)",
+        null=True,
+        blank=True,
+    )
 
-#     class Meta:
-#         ordering = ["user__first_name", "user__last_name"]
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="student_profile",
+    )
 
-#     def __str__(self) -> str:
-#         return f"{self.id}"
+    classroom = models.ForeignKey(
+        Classroom,
+        on_delete=models.CASCADE,
+        related_name="students",
+        null=True,
+        blank=True,
+    )
 
-#     @admin.display(ordering="user__first_name")
-#     def first_name(self):
-#         return self.user.first_name
+    class Meta:
+        ordering = ["user__first_name", "user__last_name"]
 
-#     @admin.display(ordering="user__last_name")
-#     def last_name(self):
-#         return self.user.last_name
+    def __str__(self) -> str:
+        return f"{self.id}"
 
-#     @admin.display(ordering="classroom__college__name")
-#     def college_name(self):
-#         return self.classroom.college.name
+    @admin.display(ordering=["user__email"])
+    def get_email(self):
+        return self.user.email
+
+    @admin.display(ordering="user__first_name")
+    def first_name(self):
+        return self.user.first_name
+
+    @admin.display(ordering="user__last_name")
+    def last_name(self):
+        return self.user.last_name
+
+    @admin.display(ordering="classroom__college__name")
+    def college_name(self):
+        return self.classroom.college.name
 
 
 # class AllowedStudents(models.Model):
