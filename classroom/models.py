7e783d478c7612bfc1e86770f3ee4185bef9f368
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-05-10 02:37:21 +0530

    Announcement Model Created

--- classroom/models.py
+++ classroom/models.py
@@ -389,26 +389,26 @@ class Subject(models.Model):
         return self.slug
 
 
-# class Announcement(models.Model):
-#     heading = models.TextField(_("Heading"), default="No Heading Given")
-#     body = models.TextField(_("Description[Optional] "), null=True, blank=True)
-#     created_at = models.DateTimeField(_("Created At "), auto_now_add=True)
-#     updated_at = models.DateTimeField(_("Updated At "), auto_now=True)
-#     subject = models.ForeignKey(
-#         Subject, on_delete=models.CASCADE, related_name="announcements"
-#     )
-#     posted_by = models.ForeignKey(
-#         Teacher, on_delete=models.CASCADE, related_name="announcements"
-#     )
+class Announcement(models.Model):
+    heading = models.TextField(_("Heading"), default="No Heading Given")
+    body = models.TextField(_("Description[Optional] "), null=True, blank=True)
+    created_at = models.DateTimeField(_("Created At "), auto_now_add=True)
+    updated_at = models.DateTimeField(_("Updated At "), auto_now=True)
+    subject = models.ForeignKey(
+        Subject, on_delete=models.CASCADE, related_name="announcements"
+    )
+    posted_by = models.ForeignKey(
+        Teacher, on_delete=models.CASCADE, related_name="announcements"
+    )
 
-#     class Meta:
-#         ordering = ["-created_at", "-updated_at"]
+    class Meta:
+        ordering = ["-updated_at", "-created_at"]
 
-#     def __str__(self) -> str:
-#         return f"{self.heading[:10]}..."
+    def __str__(self) -> str:
+        return f"{self.heading}"
 
-#     def heading_short(self):
-#         return f"{self.heading[:10]}..."
+    def heading_short(self):
+        return f"{self.heading[:10]}..."
 
 
 # class Notes(models.Model):
