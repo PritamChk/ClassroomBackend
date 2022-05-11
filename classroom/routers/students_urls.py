1bd8a0a9d25f084a08d74e70b3ba505a7d6b0211
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-05-11 11:31:06 +0530

    routers reopened

--- classroom/routers/students_urls.py
+++ classroom/routers/students_urls.py
@@ -1,51 +1,51 @@
-# from django.urls import path
-# from rest_framework_nested.routers import DefaultRouter, NestedDefaultRouter
-# from termcolor import cprint
+from django.urls import path
+from rest_framework_nested.routers import DefaultRouter, NestedDefaultRouter
+from termcolor import cprint
 
-# from ..views.student_view import (
-#     AnnouncementForStudentsViewSet,
-#     ClassroomForStudentViewSet,
-#     SemesterForStudentViewSet,
-#     StudentProfileViewSet,
-#     SubjectsForStudentsViewSet,
-#     user_category,
-# )
+from ..views.student_view import (
+    AnnouncementForStudentsViewSet,
+    ClassroomForStudentViewSet,
+    SemesterForStudentViewSet,
+    StudentProfileViewSet,
+    SubjectsForStudentsViewSet,
+    user_category,
+)
 
-# student_router = DefaultRouter()
-# student_router.register("student", StudentProfileViewSet, "student")
-
-
-# classroom_router = DefaultRouter()
-# classroom_router.register("classroom", ClassroomForStudentViewSet, "classroom")
-
-# classroom_sems_router = NestedDefaultRouter(
-#     classroom_router, "classroom", lookup="classroom"
-# )
-# classroom_sems_router.register("semester", SemesterForStudentViewSet, basename="sem")
-
-# sem_subjects_router = NestedDefaultRouter(
-#     classroom_sems_router, "semester", lookup="semester"
-# )
-# sem_subjects_router.register("subject", SubjectsForStudentsViewSet, basename="subject")
+student_router = DefaultRouter()
+student_router.register("student", StudentProfileViewSet, "student")
 
 
-# subject_announcement_router = NestedDefaultRouter(
-#     sem_subjects_router, "subject", lookup="subject"
-# )
-# subject_announcement_router.register(
-#     "announcement", AnnouncementForStudentsViewSet, basename="announcement"
-# )
+classroom_router = DefaultRouter()
+classroom_router.register("classroom", ClassroomForStudentViewSet, "classroom")
 
-# stud_urlpatterns = [path("user-type/<uuid:id>", user_category, name="user-category")]
+classroom_sems_router = NestedDefaultRouter(
+    classroom_router, "classroom", lookup="classroom"
+)
+classroom_sems_router.register("semester", SemesterForStudentViewSet, basename="sem")
+
+sem_subjects_router = NestedDefaultRouter(
+    classroom_sems_router, "semester", lookup="semester"
+)
+sem_subjects_router.register("subject", SubjectsForStudentsViewSet, basename="subject")
 
 
-# stud_urlpatterns += (
-#     student_router.urls
-#     + classroom_router.urls
-#     + classroom_sems_router.urls
-#     + sem_subjects_router.urls
-#     + subject_announcement_router.urls
-# )
+subject_announcement_router = NestedDefaultRouter(
+    sem_subjects_router, "subject", lookup="subject"
+)
+subject_announcement_router.register(
+    "announcement", AnnouncementForStudentsViewSet, basename="announcement"
+)
 
-# for url in stud_urlpatterns:
-#     cprint(url, "green")
+stud_urlpatterns = [path("user-type/<uuid:id>", user_category, name="user-category")]
+
+
+stud_urlpatterns += (
+    student_router.urls
+    + classroom_router.urls
+    + classroom_sems_router.urls
+    + sem_subjects_router.urls
+    + subject_announcement_router.urls
+)
+
+for url in stud_urlpatterns:
+    cprint(url, "green")
