9272b7ae0f0f3094bde276e0e2493154761821f8
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-05-11 11:24:30 +0530

    uncommented

--- classroom/serializers/student.py
+++ classroom/serializers/student.py
@@ -1,25 +1,25 @@
-# from rest_framework.serializers import ModelSerializer as ms
-# from classroom.models import Student, User
-# from .classroom import ClassroomReadSerializer
-# from accounts.serializers import CurrentUserSerializer
+from rest_framework.serializers import ModelSerializer as ms
+from classroom.models import Student, User
+from .classroom import ClassroomReadSerializer
+from accounts.serializers import CurrentUserSerializer
 
 
-# class StudentUserReadSerializer(ms):
-#     class Meta:
-#         model = User
-#         fields = ("first_name", "last_name", "contact_no", "email")
+class StudentUserReadSerializer(ms):
+    class Meta:
+        model = User
+        fields = ("first_name", "last_name", "contact_no", "email")
 
 
-# class StudentReadSerializer(ms):
-#     """
-#     Returns Student ID & User Profile along with Classroom Details
-#     """
+class StudentReadSerializer(ms):
+    """
+    Returns Student ID & User Profile along with Classroom Details
+    """
 
-#     user = StudentUserReadSerializer()
-#     classroom = ClassroomReadSerializer()
+    user = StudentUserReadSerializer()
+    classroom = ClassroomReadSerializer()
 
-#     class Meta:
-#         model = Student
-#         fields = ("university_roll", "user", "classroom")
-#         # read_only_fields = "__all__"
-#         # depth = 1
+    class Meta:
+        model = Student
+        fields = ("university_roll", "user", "classroom")
+        # read_only_fields = "__all__"
+        # depth = 1
