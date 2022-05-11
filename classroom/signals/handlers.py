63a6322bc9f0ba0fb2028fb9e11200e73de26d32
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-05-11 12:40:45 +0530

    AllowedTeacherClassroomLevel signal working

--- classroom/signals/handlers.py
+++ classroom/signals/handlers.py
@@ -8,6 +8,7 @@ import pandas as pd
 from classroom.models import (
     AllowedStudents,
     AllowedTeacher,
+    AllowedTeacherClassroomLevel,
     Classroom,
     College,
     Semester,
@@ -20,6 +21,7 @@ from django.core.mail import send_mail
 from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 from django.http import BadHeaderError
+
 
 @shared_task
 @receiver(post_save, sender=Classroom)
@@ -132,7 +134,7 @@ def create_allowed_teacher(sender, instance: College, created, **kwargs):
             df: pd.DataFrame = pd.read_csv(file_abs_path)
         elif str(file_abs_path).split(".")[-1] == "xlsx":
             df = pd.read_excel(file_abs_path)
-        
+
         if not "email" in df.columns:
             send_mail(
                 "Wrong File Structure",
@@ -144,8 +146,7 @@ def create_allowed_teacher(sender, instance: College, created, **kwargs):
         df_dict = df.to_dict("records")
         print(df_dict)
         list_of_teachers = [
-            AllowedTeacher(college=instance, **args)
-            for args in df.to_dict("records")
+            AllowedTeacher(college=instance, **args) for args in df.to_dict("records")
         ]
         AllowedTeacher.objects.bulk_create(list_of_teachers)
         # create_bulk_allowed_teacher.delay(df_dict, instance)
@@ -160,60 +161,155 @@ def create_allowed_teacher(sender, instance: College, created, **kwargs):
         College.objects.update(allowed_teacher_list="")
 
 
-# @receiver(post_save, sender=settings.AUTH_USER_MODEL)
-# def create_profile(sender, instance: settings.AUTH_USER_MODEL, created, **kwargs):
-#     if created:
-#         if (
-#             AllowedStudents.objects.filter(email=instance.email).exists()
-#             and not Student.objects.select_related("user")
-#             .filter(user=instance)
-#             .exists()
-#         ):
-#             classroom: Classroom = AllowedStudents.objects.get(
-#                 email=instance.email
-#             ).classroom
-#             university_roll = AllowedStudents.objects.get(
-#                 email=instance.email
-#             ).university_roll
-#             s = Student.objects.create(
-#                 university_roll=university_roll, user=instance, classroom=classroom
-#             )
-#             # TODO: Add some other info also in the mail
-#             subject = "Your Student Profile Has Been Created Successfully"
-#             msg = f"""
-#                 Student ID :{s.id}
-#                 mail : {instance.email}
-#                 classroom : {classroom.title}
-                
-#                 You Can Login After Activation Of your account
-#             """
-#             send_mail(subject, msg, settings.EMAIL_HOST_USER, [instance.email])
-#         elif (
-#             AllowedTeacher.objects.filter(email=instance.email).exists()
-#             and not Teacher.objects.select_related("user")
-#             .filter(user=instance)
-#             .exists()
-#         ):
-#             t = Teacher.objects.create(user=instance)
-#             subject = "Your Teacher Profile Has Been Created Successfully"
-#             msg = f"""
-#                 Teacher ID :{t.id}
-#                 mail : {instance.email}
-                
-#                 You Can Login After Activation Of your account
-#             """
-#             send_mail(subject, msg, settings.EMAIL_HOST_USER, [instance.email])
-#         elif instance.is_superuser or instance.is_staff:  # ADMIN
-#             print("Admin")
-#         else:
-#             subject = "Profile Creation Failed"
-#             msg = f"""
-#                 You have not been assigned any class, but your account has been created.
-#                 So to create a profile contact ADMIN
+@shared_task
+@receiver(post_save, sender=Classroom)
+def create_allowed_teacher_for_classroom_level(
+    sender, instance: Classroom, created, **kwargs
+):
+    if created:
+        if instance.allowed_teacher_list == None:
+            send_mail(
+                "Allowed Teacher List Does Not Exists",
+                "You Have To Create Allowed Teachers Manually",
+                settings.EMAIL_HOST_USER,
+                ["dba@admin.com"],  # FIXME: Send mail to session dba
+            )
+            return None
+        file_abs_path = None
+        teacher_file_path = os.path.join(
+            settings.BASE_DIR,
+            settings.MEDIA_ROOT,
+            instance.allowed_teacher_list.name,
+        )
+        if os.path.exists(teacher_file_path):
+            file_abs_path = os.path.abspath(teacher_file_path)
+        else:
+            send_mail(
+                "Allowed Teacher List Does Not Exists",
+                "You Have To Create Allowed Teachers Manually",
+                settings.EMAIL_HOST_USER,
+                ["dba@admin.com"],  # FIXME: Send mail to session dba
+            )
+            return None
 
-#                 contact mail id: {settings.EMAIL_HOST_USER}
-#             """
-#             send_mail(subject, msg, settings.EMAIL_HOST_USER, [instance.email])
+        df = None
+        if str(file_abs_path).split(".")[-1] == "csv":
+            df: pd.DataFrame = pd.read_csv(file_abs_path)
+        elif str(file_abs_path).split(".")[-1] == "xlsx":
+            df = pd.read_excel(file_abs_path)
+
+        if not "email" in df.columns:
+            send_mail(
+                "Wrong File Structure",
+                "column name should be => 'email' ",
+                settings.EMAIL_HOST_USER,
+                ["dba@admin.com"],  # FIXME: Send mail to session dba
+            )
+            return None
+        df_dict: list[dict] = df.to_dict("records")
+        print(df_dict)
+        college_allowed_teacher_list = list(
+            AllowedTeacher.objects.select_related("college")
+            .filter(college=instance.college)
+            .values_list("email", flat=True)
+        )
+        from termcolor import cprint
+
+        cprint(college_allowed_teacher_list, "red")
+        list_of_teachers = []
+        rejected_teacher_mails = []
+        for allowed_teacher in df_dict:
+            if allowed_teacher["email"] in college_allowed_teacher_list:
+                list_of_teachers.append(
+                    AllowedTeacherClassroomLevel(classroom=instance, **allowed_teacher)
+                )
+            else:
+                rejected_teacher_mails.append(allowed_teacher["email"])
+        if len(rejected_teacher_mails) > 0:
+            send_mail(
+                "Some Teacher's don't belong to the college",
+                f"""
+                Please add this teachers to college first and 
+                then add to CLASSROOM.
+                Rejected teacher list : {rejected_teacher_mails}
+                """,
+                settings.EMAIL_HOST_USER,
+                ["dba@admin.com"],
+                "",
+            )
+        # list_of_teachers = [
+        #     AllowedTeacher(classroom=instance, **args) for args in df.to_dict("records")
+        # ]
+        AllowedTeacherClassroomLevel.objects.bulk_create(list_of_teachers)
+        # create_bulk_allowed_teacher.delay(df_dict, instance)
+        email_list = df["email"].to_list()
+        subject = "Teacher Account Associated With Classroom"
+        prompt = (
+            "please use your following mail id to sign up/log in in the Classroom[LMS]"
+        )
+        try:
+            send_email_after_mass_profile_creation.delay(subject, prompt, email_list)
+        except BadHeaderError:
+            print("Could not able to sen emails to students")
+        os.remove(file_abs_path)
+        Classroom.objects.update(allowed_teacher_list="")
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_profile(sender, instance: settings.AUTH_USER_MODEL, created, **kwargs):
+    if created:
+        if (
+            AllowedStudents.objects.filter(email=instance.email).exists()
+            and not Student.objects.select_related("user")
+            .filter(user=instance)
+            .exists()
+        ):
+            classroom: Classroom = AllowedStudents.objects.get(
+                email=instance.email
+            ).classroom
+            university_roll = AllowedStudents.objects.get(
+                email=instance.email
+            ).university_roll
+            s = Student.objects.create(
+                university_roll=university_roll, user=instance, classroom=classroom
+            )
+            # TODO: Add some other info also in the mail
+            subject = "Your Student Profile Has Been Created Successfully"
+            msg = f"""
+                Student ID :{s.id}
+                mail : {instance.email}
+                classroom : {classroom.title}
+
+                You Can Login After Activation Of your account
+            """
+            send_mail(subject, msg, settings.EMAIL_HOST_USER, [instance.email])
+        elif (
+            AllowedTeacher.objects.filter(email=instance.email).exists()
+            and not Teacher.objects.select_related("user")
+            .filter(user=instance)
+            .exists()
+        ):
+            t = Teacher.objects.create(user=instance)
+            subject = "Your Teacher Profile Has Been Created Successfully"
+            msg = f"""
+                Teacher ID :{t.id}
+                mail : {instance.email}
+
+                You Can Login After Activation Of your account
+            """
+            send_mail(subject, msg, settings.EMAIL_HOST_USER, [instance.email])
+        elif instance.is_superuser or instance.is_staff:  # ADMIN
+            print("Admin")
+        else:
+            subject = "Profile Creation Failed"
+            msg = f"""
+                You have not been assigned any class, but your account has been created.
+                So to create a profile contact ADMIN
+
+                contact mail id: {settings.EMAIL_HOST_USER}
+            """
+            send_mail(subject, msg, settings.EMAIL_HOST_USER, [instance.email])
+
 
 @shared_task
 @receiver(post_delete, sender=Student)
