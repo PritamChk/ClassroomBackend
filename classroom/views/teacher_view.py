08e7cc181cd64c95e643792414b86ae5af12879d
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-05-16 02:44:44 +0530

    uncomment FileUploadPerser

--- classroom/views/teacher_view.py
+++ classroom/views/teacher_view.py
@@ -152,11 +152,15 @@ class TeacherNotesUploadViewSet(ModelViewSet):
         }
 
 
+# from rest_framework.parsers import FileUploadParser, JSONParser
+
+
 class FileUploadDeleteViewSet(ModelViewSet):
     http_method_names = ["get", "post", "patch", "delete", "options", "head"]
     my_tags = ["[teacher] 6.2 upload/delete attached notes file"]
     serializer_class = NotesFileUploadByTeacherSerializer
     lookup_field = "title"
+    # parser_classes = [FileUploadParser, JSONParser]
 
     def get_serializer_context(self):
         return {"notes_slug": self.kwargs.get("notes_slug")}
