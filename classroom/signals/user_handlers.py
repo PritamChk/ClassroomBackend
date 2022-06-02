00e62cb5aa238324664d55f1a252c3e53a5bbe7c
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-06-02 16:05:59 +0530

    mail send on profile deletion

--- classroom/signals/user_handlers.py
+++ classroom/signals/user_handlers.py
@@ -8,6 +8,15 @@ def delete_user_on_student_delete(sender, instance: Student, **kwargs):
         user = User.objects.filter(pk=instance.user.id)
         if user.exists():
             user.delete()
+            try:
+                subject = "You Have Been Removed from Classroom"
+                body = f"""
+                    your mail - {instance.user.email} is no more associated with any college or classroom
+                """
+            except:
+                cprint(
+                    "student profile deletion confirmation mail sending failed", "red"
+                )
     except:
         pass
 
@@ -19,6 +28,13 @@ def delete_user_on_dba_delete(sender, instance: CollegeDBA, **kwargs):
         user = User.objects.filter(pk=instance.user.id)
         if user.exists():
             user.delete()
+            try:
+                subject = "Your DBA Profile Have Been Removed from College"
+                body = f"""
+                    your mail - {instance.user.email} is no more associated with any college or classroom
+                """
+            except:
+                cprint("admin profile deletion confirmation mail sending failed", "red")
     except:
         pass
 
@@ -33,5 +49,15 @@ def delete_user_on_teacher_delete(sender, instance: Teacher, **kwargs):
             user = User.objects.filter(pk=instance.user.id)
             if user.exists():
                 user.delete()
+                try:
+                    subject = "Your Teacher Profile Have Been Removed from College"
+                    body = f"""
+                        your mail - {instance.user.email} is no more associated with any college or classroom
+                    """
+                except:
+                    cprint(
+                        "teacher profile deletion confirmation mail sending failed",
+                        "red",
+                    )
     except:
         pass
